1710f866e8c3898b70463efb6e4ed822e9d1b895
Walter Irvin <43546119+WalterIrvin@users.noreply.github.com>
2020-01-24 23:18:18 -0500

    -The keybind script will now non-block wait for input to set axis, still implementing actual axis change for unity input.

--- Assets/Scripts/Systems/Misc/EventSystemInputManager.cs
+++ Assets/Scripts/Systems/Misc/EventSystemInputManager.cs
@@ -11,9 +11,20 @@ public class EventSystemInputManager : MonoBehaviour
     /// will update the input axis to allow only that gamepad to control the ui / build commands.
     /// </summary>
     public StandaloneInputModule m_inputRef;
+    private bool m_paused = false;
     void Start()
     {
         UpdateInputManager();
+
+    }
+    public void TogglePause()
+    {
+        //Toggles the pause-state of the inputmanager
+        m_paused = !m_paused;
+        if (m_paused)
+            PauseInputManager();
+        else
+            ResumeInputManager();
     }
     public void UpdateInputManager()
     {
@@ -28,5 +39,25 @@ public class EventSystemInputManager : MonoBehaviour
             GameObject first = EventSystem.current.firstSelectedGameObject;
             EventSystem.current.SetSelectedGameObject(first);
         }
+    }
+    public void ResumeInputManager()
+    {
+        //Returns InputManager to last state, doesn't reset the currently selected item
+        m_inputRef.submitButton = "P" + BuildSubjectLogic.m_currentPlayerId + "_Submit";
+        m_inputRef.cancelButton = "P" + BuildSubjectLogic.m_currentPlayerId + "_Cancel";
+        m_inputRef.horizontalAxis = "P" + BuildSubjectLogic.m_currentPlayerId + "_Horizontal";
+        m_inputRef.verticalAxis = "P" + BuildSubjectLogic.m_currentPlayerId + "_Vertical";
+    }
+    public void PauseInputManager()
+    {
+        //Effectively disables all input when setting keybindings, this prevents moving off button
+        m_inputRef.submitButton = "Null";
+        m_inputRef.cancelButton = "Null";
+        m_inputRef.horizontalAxis = "Null";
+        m_inputRef.verticalAxis = "Null";
+    }
+    public static void ToggleInput()
+    {
+        EventSystem.current.gameObject.GetComponent<EventSystemInputManager>().TogglePause();
     }
 }
--- Assets/Scripts/Systems/Misc/Keybinder.cs
+++ Assets/Scripts/Systems/Misc/Keybinder.cs
@@ -1,96 +1,118 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
 
 public class Keybinder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    private TextMeshProUGUI m_textRef;
+    private IEnumerator m_coroutine;
     private IEnumerator KeyWait()
-    {
-        yield return GetKeyPress();
-    }
-    private IEnumerator GetKeyPress()
     {
         float pid = BuildSubjectLogic.m_currentPlayerId;
         bool done = false;
-        while(!done)
+        while (Input.GetKeyDown("joystick " + pid + " button 0"))
+        {
+            //Wait for player to release selection button before proceeding
+            yield return null;
+        }
+        while (!done)
         {
             // Key checker 
             if (Input.GetKeyDown("joystick " + pid + " button 0"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             if (Input.GetKeyDown("joystick " + pid + " button 1"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             if (Input.GetKeyDown("joystick " + pid + " button 2"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             if (Input.GetKeyDown("joystick " + pid + " button 3"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             if (Input.GetKeyDown("joystick " + pid + " button 4"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             if (Input.GetKeyDown("joystick " + pid + " button 5"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             if (Input.GetKeyDown("joystick " + pid + " button 6"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             if (Input.GetKeyDown("joystick " + pid + " button 7"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             if (Input.GetKeyDown("joystick " + pid + " button 8"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             if (Input.GetKeyDown("joystick " + pid + " button 9"))
             {
+                Debug.Log("Got button press");
                 done = true;
             }
             // Axis Checker
-            if (Input.GetAxis("X" + pid + "_axis") > 0.3)
+            if (Mathf.Abs(Input.GetAxis("X" + pid + "_axis")) > 0.3)
             {
+                Debug.Log("X axis");
                 done = true;
             }
-            if (Input.GetAxis("Y" + pid + "_axis") > 0.3)
+            if (Mathf.Abs(Input.GetAxis("Y" + pid + "_axis")) > 0.3)
             {
+                Debug.Log("Y axis");
                 done = true;
             }
-            if (Input.GetAxis("RX" + pid + "_axis") > 0.3)
+            if (Mathf.Abs(Input.GetAxis("RX" + pid + "_axis")) > 0.3)
             {
+                Debug.Log("RX axis");
                 done = true;
             }
-            if (Input.GetAxis("RY" + pid + "_axis") > 0.3)
+            if (Mathf.Abs(Input.GetAxis("RY" + pid + "_axis")) > 0.3)
             {
+                Debug.Log("RY axis");
                 done = true;
             }
-            if (Input.GetAxis("T" + pid + "_axis") > 0.3)
+            if (Mathf.Abs(Input.GetAxis("T" + pid + "_axis")) > 0.3)
             {
+                Debug.Log("T axis");
                 done = true;
             }
+            yield return null;
         }
-        return null;
+        EventSystemInputManager.ToggleInput();
+    }
+    public void SetTextRef(TextMeshProUGUI textRef)
+    {
+        m_textRef = textRef;
     }
     public void SetInputBinding(string newInputAxis)
     {
-        KeyWait();
+        //Sets the binding sent it to P(pid)_axis
+        Debug.Log("Test");
+        m_coroutine = KeyWait();
+        StartCoroutine(m_coroutine);
+        //Toggles the paused state for input controller while waiting for keybind input
+        EventSystemInputManager.ToggleInput();
     }
+    
 }
+
