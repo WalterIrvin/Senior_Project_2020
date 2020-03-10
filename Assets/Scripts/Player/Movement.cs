bba88d55597b48d7fac7902b2c01f186e09dab61
WalterIrvin <walterdirvin@gmail.com>
2020-03-09 21:26:01 -0400

    -Fixed up the hyperspace effect a bit, now actually works in reverse

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -10,6 +10,10 @@ public class Movement : MonoBehaviour
     public Image m_fillBar;
     public AudioSource m_thrustSound; // plays when thrusting
     public List<ParticleSystem> m_thrusterPlumes;
+    public ParticleSystem m_hyperspace;
+    public ParticleSystem m_hyperspace_b;
+    private ParticleSystem.MainModule m_hype_b;
+    private ParticleSystem.MainModule m_hype;
     private ParticleSystem.MainModule m_tlplume;  //This is beyond annoying, but the way start speed works means these variables must be class level
     private ParticleSystem.MainModule m_trplume;  //They don't take effect from the looks of it when unless they are pre-declared
     private ParticleSystem.MainModule m_blplume;
@@ -24,7 +28,7 @@ public class Movement : MonoBehaviour
     private float m_turning_accel = 297.69f; // amt to add per deltaTime
     private float m_angle_friction = 19.7f; // turning friction
     //Movement accel vars
-    private float m_acceleration_rate = 7000; // rate at which to increase acceleration to max 
+    private float m_acceleration_rate = 18000; // rate at which to increase acceleration to max 
     private float m_max_vel = 30000;
     private float m_cur_vel = 0;
 
@@ -34,6 +38,9 @@ public class Movement : MonoBehaviour
     private float m_axis_thrust = 0;
     private int m_joy_num = 1; //Change
     private bool m_colliding = false;
+    private bool m_tooslow = true; // ship going too slow for hyperspace
+    private bool m_tooslow_b = true; // ship not reversing fast enough for back-space
+
     private void Start()
     {
         m_tlplume = m_thrusterPlumes[0].main;
@@ -41,6 +48,8 @@ public class Movement : MonoBehaviour
         m_blplume = m_thrusterPlumes[2].main;
         m_brplume = m_thrusterPlumes[3].main;
         m_midplume = m_thrusterPlumes[4].main;
+        m_hype = m_hyperspace.main;
+        m_hype_b = m_hyperspace_b.main;
     }
     public void SetController(int num)
     {
@@ -148,10 +157,10 @@ public class Movement : MonoBehaviour
     {
         m_colliding = false;
     }
-    private void UpdatePlumes(float vel)
+    private void UpdatePlumes()
     {
         float percentage = m_max_plume_len / m_max_vel; // scales the vel down so that it fits in the 0-8 range
-        float plume_len = vel * percentage;
+        float plume_len = m_cur_vel * percentage;
         if (plume_len < 0)
         {
             plume_len = 0;
@@ -171,11 +180,64 @@ public class Movement : MonoBehaviour
             }
         }
     }
+    private void UpdateHyperspace()
+    {
+        // Gets the magnitude of the distance travelled from old position to cur position
+        // The 180 is because the max tested speed is around 5, and 180 * 5 = 900, which is about what we want the particle system to go at
+        float percent = 9000 / m_max_vel; // scales the vel down so that it fits in the 0-900 range
+        float cur_velocity = m_cur_vel * percent;
+        float min_speed = 5000; // min speed to show particles
+        if (cur_velocity < 0)
+        {
+            //If ship is going backwards, play the reverse particle system
+            cur_velocity = Mathf.Abs(cur_velocity);
+            m_hype.startSpeed = cur_velocity;
+            Debug.Log(cur_velocity);
+            if (m_hype.startSpeed.constant < min_speed)
+            {
+                //Clears the particle system if speed is too low
+                m_hyperspace_b.Clear();
+                m_hyperspace_b.Stop();
+                m_tooslow_b = true;
+            }
+            else if (m_hype.startSpeed.constant > min_speed)
+            {
+                if (m_tooslow_b)
+                {
+                    // Start back up animation when ship speeds up
+                    m_hyperspace_b.Play();
+                    m_tooslow_b = false;
+                }
+            }
+        }
+        else
+        {
+            m_hype.startSpeed = cur_velocity;
+            if (m_hype.startSpeed.constant < min_speed)
+            {
+                //Clears the particle system if speed is too low
+                m_hyperspace.Clear();
+                m_hyperspace.Stop();
+                m_tooslow = true;
+            }
+            else if (m_hype.startSpeed.constant > min_speed)
+            {
+                if (m_tooslow)
+                {
+                    // Start back up animation when ship speeds up
+                    m_hyperspace.Play();
+                    m_tooslow = false;
+                }
+            }
+        }
+       
+    }
     public void FixedUpdate()
     {
         GetInput();
         MovementUpdate();
-        UpdatePlumes(m_cur_vel);
+        UpdatePlumes();
+        UpdateHyperspace();
         if (m_cur_vel < 0)
         {
             m_fillBar.color = new Color(1, 0, 0);
--- Assets/Scripts/Systems/Misc/CameraFollow.cs
+++ Assets/Scripts/Systems/Misc/CameraFollow.cs
@@ -10,13 +10,10 @@ public class CameraFollow : MonoBehaviour
     public Transform m_target;
     public Transform m_offset;
     public float m_smooth_time = 10.125f; // higher value, more smoothing 0...1
-    public ParticleSystem m_hyperspace; // The warp particle system
-    private ParticleSystem.MainModule m_hype; // main module for hyperspace
     private float m_angle_per_sec = 10f;
     private Vector3 m_velocity = Vector3.zero;
     private List<FocusIcon> m_focuserList = new List<FocusIcon>();
     private List<TargetIconLocator> m_pointerList = new List<TargetIconLocator>();
-    private bool m_tooslow = true; // ship going too slow for hyperspace
 
     void Start()
     {
@@ -33,7 +30,6 @@ public class CameraFollow : MonoBehaviour
             pointer.Init(m_canvas, core.gameObject);
             m_pointerList.Add(pointer);
         }
-        m_hype = m_hyperspace.main;
     }
     void Update()
     {
@@ -48,26 +44,6 @@ public class CameraFollow : MonoBehaviour
         {
             transform.rotation = smoothed_rotation;
         }
-        // Gets the magnitude of the distance travelled from old position to cur position
-        // The 180 is because the max tested speed is around 5, and 180 * 5 = 900, which is about what we want the particle system to go at
-        float raw_vel = (transform.position - old_pos).magnitude;
-        float cur_velocity = raw_vel * 180;
-        m_hype.startSpeed = cur_velocity;
-        if (m_hype.startSpeed.constant < 200)
-        {
-            //Clears the particle system if speed is too low
-            m_hyperspace.Clear();
-            m_hyperspace.Stop();
-            m_tooslow = true;
-        }
-        else if (m_hype.startSpeed.constant > 200)
-        {
-            if (m_tooslow)
-            {
-                // Start back up animation when ship speeds up
-                m_hyperspace.Play();
-                m_tooslow = false;
-            }
-        }
+        
     }
 }
